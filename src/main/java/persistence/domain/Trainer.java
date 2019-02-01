2b323a0f2ccf6f36700f0f42391dafceb10b32b5
anitachandlermohan <anitachandlermohan@gmail.com>
2019-02-01 10:46:25 +0000

    completed trainer class

--- src/main/java/persistence/domain/Trainer.java
+++ src/main/java/persistence/domain/Trainer.java
@@ -1,15 +1,51 @@
 package persistence.domain;
 
+import java.util.Collection;
+
 import javax.persistence.*;
 
 public class Trainer {
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
-	private Long id;
+	private Long trainerID;
 	@Column
-	private String firstName;
-	@Column
-	private String lastName;
+	private String trainerName;
+	
+	@OneToMany(mappedBy="trainer_trainees")
+	private Collection<Trainee> trainees;
 	
 	
+	public Trainer() {
+		
+	}
+	
+	public Trainer(String trainerName, Collection<Trainee> trainees) {
+		this.trainerName = trainerName;
+		this.trainees = trainees;
+		
+	}
+	
+	public String getTrainerName() {
+		return trainerName;
+	}
+	
+	public void setTrainerName(String trainerName) {
+		this.trainerName = trainerName;
+	}
+	
+	public Long getTrainerID(){
+		return trainerID;
+	}
+	
+	public void setTrainerID(Long trainerID) {
+		this.trainerID = trainerID;
+	}
+	
+	public Collection<Trainee> getTrainees(){
+		return trainees;
+	}
+	
+	public void setTrainees(Collection<Trainee> trainees) {
+		this.trainees = trainees;
+	}
 
 }
